fe86bd40ab9cac5da378b64c66c031728071d1b0
AzizBO <aziz.abderrahmane.ben.othmane@tum.de>
2026-03-14 13:40:10 +0100

    new version

--- backend/app/frontend.tsx
+++ backend/app/frontend.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useMemo, useState } from 'react';
 
 function Badge({ children, tone = 'default' }) {
--- backend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>OlySublet Backend Running 🚀</h1>
-    </div>
-  );
-}
